77bd4d3dc9202b670939e99b81a553d63adde5d7
pfbrodon <pfbrodon@gmail.com>
2023-07-12 00:22:46 -0300

    commit 10 mac

--- Main.py
+++ Main.py
@@ -325,17 +325,15 @@ btn9.place(x=520,y=130)
 ################################################################################
 ###TREE VIEW- TABLA#############################################################
 style.theme_use("default")
+tablaFerreteria=ttk.Treeview(height=20,
+                             show='tree headings',
+                             columns=('#0', '#1','#2','#3','#4'))
 style.configure("Treeview",
                 background="silver",
                 foreground="black",
                 rowheight=25,
                 fieldbackground="silver"
                 )
-#style.map("Treeview",=[('select','green')])
-tablaFerreteria=ttk.Treeview(height=20,
-                             show='tree headings',
-                             columns=('#0', '#1','#2','#3','#4'))
-tablaFerreteria=ttk.Treeview(ventana)
 tablaFerreteria.place(x=10,y=250,width=700,height=400)
 tablaFerreteria.column('#0', width=20,anchor='e')
 tablaFerreteria.heading('#0',text="CODIGO",anchor='center',)
